1a479a49bc8f3e61ef0475b4c4ff25a800a56476
Nathan Caron <caron.nathan@hotmail.com>
2022-07-15 11:21:59 +0200

    Chap: Les fonctions

--- demo.php
+++ demo.php
@@ -1,36 +1,16 @@
 <?php
-$horaires = [];
-
-while (true) {
-  $debut = (int)readline("Entrez une heure d'ouverture : ");
-  $fin = (int)readline("Entrez une heure de fermeture : ");
-
-  if ($debut >= $fin) echo "L'heure d'ouverture doit être strictement inférieur à l'heure de fermteure\n";
-  else {
-    $horaires[] = [$debut, $fin];
-    $action = readline("Voulez-vous rajouter des horaires (o/n) : ");
-
-    if ($action === 'n') break;
-  }
+$insultes = ['merde', 'con'];
+$insultes_censure = [];
+foreach ($insultes as $insulte) {
+  $insultes_censure[] = substr($insulte, 0, 1) . str_repeat('*', strlen($insulte) - 1);
 }
 
-echo 'Le magasin est ouvert de ';
-foreach ($horaires as $key => $horaire) {
-  if ($key > 0) echo ' et de ';
-  echo $horaire[0] . 'h à ' . $horaire[1] . 'h';
-}
+$phrase = strtolower(readline('Entrez une phrase : '));
 
-echo PHP_EOL;
+$phrase = str_replace($insultes, $insultes_censure, $phrase);
 
-// $heure = (int)readline('Heure de visite du magasin : ');
-// $ouvert = false;
-
-// foreach ($horaires as $horaire) {
-//   if ($heure >= $horaire[0] && $heure <= $horaire[1]) {
-//     $ouvert = true;
-//     break;
-//   }
+// foreach ($insultes as $insulte) {
+//   $phrase = str_replace($insulte, str_repeat('*', strlen($insulte)), $phrase);
 // }
 
-// if ($ouvert) echo 'Le magasin est ouvert';
-// else echo 'Le magasin est fermé';
+echo $phrase . PHP_EOL;
