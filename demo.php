30579e7114858f9176763966643baf1329fd40e6
Nathan Caron <caron.nathan@hotmail.com>
2022-07-15 08:35:02 +0200

    Chap: Les conditions

--- demo.php
+++ demo.php
@@ -1,15 +1,26 @@
 <?php
-$classe = [
-  [
-    'prenom' => 'Marc',
-    'nom' => 'Doe',
-    'notes' => [10, 20, 14],
-  ],
-  [
-    'prenom' => 'Jean',
-    'nom' => 'Doe',
-    'notes' => [7, 18, 13],
-  ],
-];
+$heure = (int)readline('Entrez une heure : ');
 
-echo $classe[1]['notes'][2];
+if (($heure >= 9 && $heure <= 12) || ($heure >= 14 && $heure <= 17)) {
+  echo 'Le magasin est ouvert';
+} else {
+  echo 'Le magasin est fermé';
+}
+
+// switch ($action) {
+//   case 1:
+//     echo "J'attaque";
+//     break;
+
+//   case 2:
+//     echo 'Je défends';
+//     break;
+
+//   case 3:
+//     echo 'Je passes mon tour';
+//     break;
+
+//   default:
+//     echo 'Commande inconnue';
+//     break;
+// }
