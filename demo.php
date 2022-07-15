95c695e999be76382a834d3dd5e007725f82a4f3
Nathan Caron <caron.nathan@hotmail.com>
2022-07-15 10:35:45 +0200

    Chap: Les boucles

--- demo.php
+++ demo.php
@@ -1,26 +1,36 @@
 <?php
-$heure = (int)readline('Entrez une heure : ');
+$horaires = [];
 
-if (($heure >= 9 && $heure <= 12) || ($heure >= 14 && $heure <= 17)) {
-  echo 'Le magasin est ouvert';
-} else {
-  echo 'Le magasin est fermé';
+while (true) {
+  $debut = (int)readline("Entrez une heure d'ouverture : ");
+  $fin = (int)readline("Entrez une heure de fermeture : ");
+
+  if ($debut >= $fin) echo "L'heure d'ouverture doit être strictement inférieur à l'heure de fermteure\n";
+  else {
+    $horaires[] = [$debut, $fin];
+    $action = readline("Voulez-vous rajouter des horaires (o/n) : ");
+
+    if ($action === 'n') break;
+  }
 }
 
-// switch ($action) {
-//   case 1:
-//     echo "J'attaque";
-//     break;
+echo 'Le magasin est ouvert de ';
+foreach ($horaires as $key => $horaire) {
+  if ($key > 0) echo ' et de ';
+  echo $horaire[0] . 'h à ' . $horaire[1] . 'h';
+}
 
-//   case 2:
-//     echo 'Je défends';
-//     break;
+echo PHP_EOL;
 
-//   case 3:
-//     echo 'Je passes mon tour';
-//     break;
+// $heure = (int)readline('Heure de visite du magasin : ');
+// $ouvert = false;
 
-//   default:
-//     echo 'Commande inconnue';
+// foreach ($horaires as $horaire) {
+//   if ($heure >= $horaire[0] && $heure <= $horaire[1]) {
+//     $ouvert = true;
 //     break;
+//   }
 // }
+
+// if ($ouvert) echo 'Le magasin est ouvert';
+// else echo 'Le magasin est fermé';
