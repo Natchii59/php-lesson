1c365e10a3fffb0fd022d915175cf814795b0b1d
Nathan Caron <caron.nathan@hotmail.com>
2022-07-15 19:03:19 +0200

    Chap: Traitement des formulaires

--- functions.php
+++ functions.php
@@ -16,5 +16,26 @@ function nav_menu(string $linkClass = ''): string
 {
   return
     nav_item('/index.php', 'Accueil', $linkClass) .
-    nav_item('/contact.php', 'Contact', $linkClass);
+    nav_item('/contact.php', 'Contact', $linkClass) .
+    nav_item('/jeu.php', 'Jeu', $linkClass);
+}
+
+function checkbox(string $name, string $value, array $datas): string
+{
+  $attributes = '';
+  if (isset($datas[$name]) && in_array($value, $datas[$name])) $attributes .= 'checked';
+
+  return <<<HTML
+    <input type="checkbox" name="{$name}[]" id="$value" value="$value" $attributes />
+  HTML;
+}
+
+function radio(string $name, string $value, array $datas): string
+{
+  $attributes = 'required';
+  if (isset($datas[$name]) && $datas[$name] === $value) $attributes .= ' checked';
+
+  return <<<HTML
+    <input type="radio" name="$name" id="$value" value="$value" $attributes />
+  HTML;
 }
--- menu.php
+++
@@ -1,5 +0,0 @@
-<?php
-?>
-
-<?= nav_item('/index.php', 'Accueil', $class) ?>
-<?= nav_item('/contact.php', 'Contact', $class) ?>
