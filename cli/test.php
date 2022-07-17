15c9edea660a0aa77c309e22c47d4caf423a7d03
Nathan Caron <caron.nathan@hotmail.com>
2022-07-17 17:30:01 +0200

    Chap: Les class

--- cli/test.php
+++ cli/test.php
@@ -1,7 +1,7 @@
 <?php
-$d = '2010-04-01';
-$d2 = '2022-06-02';
-
-$date = new DateTime($d);
-$date2 = new DateTime($d2);
-echo $date->diff($date2, true)->format('%d days, %m months, %y years') . PHP_EOL;
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Creneau.php';
+$creneau = new Creneau(9, 12);
+$creneau2 = new Creneau(14, 18);
+var_dump($creneau->inclusHeure(10));
+var_dump($creneau->intersect($creneau2));
+echo $creneau;
