1ee96768ef010f7aa7ea510477ae84fb189eb0c3
Nathan Caron <caron.nathan@hotmail.com>
2022-07-17 17:52:31 +0200

    Chap: Statique

--- cli/test.php
+++ cli/test.php
@@ -1,7 +1,3 @@
 <?php
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Creneau.php';
-$creneau = new Creneau(9, 12);
-$creneau2 = new Creneau(14, 18);
-var_dump($creneau->inclusHeure(10));
-var_dump($creneau->intersect($creneau2));
-echo $creneau;
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Form.php';
+echo Form::checkbox('Test');
